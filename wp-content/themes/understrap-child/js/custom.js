51c6f5ac4d62bd0ef3f268e176be3faa3483d5ba
Florian Desmet <desmetflorian@outlook.fr>
2020-04-01 14:49:19 +0200

    fichier custom.js

--- wp-content/themes/understrap-child/js/custom.js
+++ wp-content/themes/understrap-child/js/custom.js
@@ -1,1 +1,10 @@
-alert("Hello !!");
+if (window.matchMedia("(min-width: 600px)").matches) {
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 500) {
+            $('#wrapper-navbar').addClass('fixed-header');
+        }
+        else {
+            $('#wrapper-navbar').removeClass('fixed-header');
+        }
+    });
+}
